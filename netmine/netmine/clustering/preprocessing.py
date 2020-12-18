cd44f7ac99267b1af41e0f713013048a7c3ae144
fathelrahman <f.a.a_114100@gmail.com>
2020-12-18 16:34:27 +0200

    base scaler class and sigmoid based scaling class were added

--- netmine/netmine/clustering/preprocessing.py
+++ netmine/netmine/clustering/preprocessing.py
@@ -173,3 +173,158 @@ class IgnoreNanValuesHandler(NanValuesHandler):
         dataset = np.insert(dataset, obj=self.nan_rows_indices - np.arange(self.nan_rows_indices.shape[0]),
                             values=extended_nan_rows, axis=0)
         return dataset
+
+class Scaler:
+    """
+    Base class for scaler classes
+    
+    Methods
+    -------
+    fit : None
+        prepares the scaling parameters according to the dataset
+    handle : ndarray
+        scales a dataset based on the parameters specified in fit
+    fit_handle : ndarray
+        prepares the scaling parameters and scale a datatset
+    inverse_handle : ndarray
+        inversts the effect of handle        
+    """
+
+    def fit(self, dataset):
+        """
+        prepares the scaling parameters according to the dataset
+        
+        This method should be overriden in any class that extends this class.
+
+        :param ndarray dataset: the dataset to set the parameters according to 
+        """
+        pass
+
+    def handle(self, dataset):
+        """
+        scales a dataset based on the parameters specified in fit
+        
+        This method should be overriden in any class that extends this class. This method should not be
+        called before calling fit with a dataset with similar features. Similar features is specified 
+        according to the specific scaling method.
+
+        :param ndarray dataset: The dataset to be scaled.
+        :return ndarray: The scaled version of the dataset. 
+        """
+        pass
+
+    def fit_handle(self, dataset):
+        """
+        prepares the scaling parameters and scale a datatset
+        
+        Calling this method is equivalent to calling fit and then handle with the same dataset.
+
+        :param ndarray dataset: The dataset to be scaled.
+        :return ndarray: The scaled version of the dataset.
+        """
+        self.fit(dataset)
+        return self.handle(dataset)
+
+    def inverse_handle(self, dataset):
+        """
+        inversts the effect of handle
+        
+        This method should be overriden in any class that extends this class. calling this method on 
+        the result of handle should returns the original dataset passed to handle.
+
+        :param ndarray dataset: the scaled dataset whose scaling to be inverted
+        :return ndarray: the recoovered dataset 
+        """
+        pass
+
+class SigmoidWith10pecrentScaler(Scaler):
+    """
+    A scaler class that uses sigmoid function
+    
+    This class multiplies the dataset by a sigmoid function such that 2 specific points are mapped to
+    approximately 10% and 90% - of the entire resulting range - after scaling.
+    
+    Methods
+    -------
+    
+            
+    """
+
+    def __init__(self, a10=10, a90=90):
+        """
+        :param a10: The value/values to be mapped to 10% point in the resulting range. If the dataset
+        to be scaled contains many columns, then, each coulumn may have a different value for a10 and
+        a90; therefore, a10 and a190 are stored as ndarrays with a length equals to the number of the
+        columns. Or, if the values are the same regardless of the column used, a single values for
+        a10 and a90 can be used.
+        :param a90: The value/values to be mapped to 10% point in the resulting range. An argument
+        similar to that written for a10 applies here.
+        """
+        self.a10 = a10
+        self.a90 = a90
+        self._alpha = 0
+        self._beta = 1
+
+    def fit(self, dataset):
+        """
+        prepares _alpha and _beta parameters according to a10 and a90
+
+        _alpha and _beta are used as parameters for the sigmoid function. They are set here such that
+        a10 values give .1 and a90 values give .9 after scaling. This method shoud be calld whenever
+        the values of a10 or a90 changes.
+
+        :param dataset: this is passed for compatibility reasons
+        :return:
+        """
+        self._alpha = (self.a10 + self.a90) / 2.
+        self._beta = (self.a90 - self._alpha) / 2.
+        
+    def handle(self, dataset):
+        """
+        Scales the dataset using a sigmoid function
+
+        This method scales a dataet using the parameters set in fit. It overrides the base class
+        method. It should not be called before calling fit.
+
+        :param ndarray dataset: The dataset to be scaled
+        :return ndarray: The scaled version of the dataset
+        """
+        dataset = dataset.copy()
+        dataset = self.map_values(dataset)
+        dataset = 1 / (1 + np.exp(-1 * dataset))
+        return dataset
+
+    def inverse_handle(self, dataset):
+        """
+        inversts the effect of handle using the inverse of the sigmoid function
+
+        This method scales a dataet using the parameters set in fit. It overrides the base class
+        method.
+
+        :param ndarray dataset: the scaled dataset whose scaling to be inverted
+        :return ndarray: the recoovered dataset
+        """
+        dataset = dataset.copy()
+        dataset = np.log((1 / dataset) - 1) * -1
+        dataset = self.inverse_map_values(dataset)
+        return dataset
+
+    def map_values(self, x):
+        """
+        Linearly maps vaues using _alpha and _beta parameters
+
+        :param int or ndarray x: The value/values to be mapped.
+        :return: A mapped version of the dataset.
+        """
+        return (x - self._alpha) / self._beta
+
+    def inverse_map_values(self, x):
+        """
+        Inversts the linear mapping made by map_values
+
+        _alpha and _beta should not change between calling map_values and calling this method.
+
+        :param int or ndarray x: The value/values to be mapped.
+        :return: A mapped version of the dataset.
+        """
+        return x * self._beta + self._alpha
